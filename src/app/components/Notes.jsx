ce098de161373278158307d2def0d0aa318f0d36
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-02-15 21:17:59 +0530

    feat: Add note feature added

--- src/app/components/Notes.jsx
+++ src/app/components/Notes.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 
-const Notes = () => {
+const Notes = ({text, id}) => {
   return (
     <div className="bg-yellow-200 min-h-[250px] rounded-md shadow-md p-5 flex flex-col justify-between">
-      <p className="text-gray-800">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla
-        accusamus molestiae eius, nesciunt fugiat facere dolorum, impedit
-        asperiores corporis animi aut ea. Harum excepturi et ex dolore a vitae
-        sequi!
-      </p>
+      <p className="text-gray-800">{text}</p>
       <div className="text-right">
-        <button className="text-lg bg-gray-100 border-2 border-gray-400 p-1 rounded-lg">🗑️</button>
+        <button className="text-lg bg-gray-100 border-2 border-gray-400 p-1 rounded-lg">
+          🗑️
+        </button>
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,30 @@
 
 import { useState } from "react";
 import Notes from "./components/Notes";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [noteMsg, setNoteMsg] = useState("");
+
+  //Add Notes
+  const addNote = () => {
+    if (noteMsg.trim()) {
+      // Generate a unique ID for each note
+      const newNote = { id: uuidv4(), text: noteMsg };
+      setNotes([...notes, newNote]);
+      setNoteMsg("");
+    }
+  };
+
+  //Clear input field when click on cancel if text is exists
+  const clearField = () => {
+    if (noteMsg.trim()) {
+      setIsOpen(false);
+      setNoteMsg("");
+    }
+  };
 
   return (
     <div className="px-20 py-10">
@@ -34,22 +55,27 @@ export default function Home() {
           type="text"
           className="w-full rounded-lg shadow-md px-4 py-2 border border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none text-gray-700"
           placeholder="Type Your Notes...."
+          value={noteMsg}
+          onChange={(e) => setNoteMsg(e.target.value)}
         />
-        <button className="text-white bg-green-600 w-fit px-4 py-2 rounded-full hover:bg-green-700">
+        <button
+          className="text-white bg-green-600 w-fit px-4 py-2 rounded-full hover:bg-green-700"
+          onClick={addNote}
+        >
           Save
         </button>
         <button
           className="text-white bg-red-600 w-fit px-4 py-2 rounded-full hover:bg-red-700"
-          onClick={() => setIsOpen(false)}
+          onClick={clearField}
         >
           Cancel
         </button>
       </div>
 
       <div className="mt-10 px-10 grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-10">
-        <Notes />
-        <Notes />
-        <Notes />
+        {notes.map((note) => (
+          <Notes key={note.id} id={note.id} text={note.text} />
+        ))}
       </div>
     </div>
   );
